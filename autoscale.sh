f978cd45aecbe5860ce4d42a3db077854219cd5e
Mohd Rayees <mailtomdrayees@gmail.com>
2026-01-17 06:22:24 +0530

    made corrections

--- autoscale.sh
+++ autoscale.sh
@@ -1,10 +1,7 @@
-#Use this script while creating launch Template for Autoscaling Task
-
 #!/bin/bash
 set -e
 sudo apt update
 
-# Get IMDSv2 token
 TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" \
   -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 
@@ -15,9 +12,7 @@ HOSTNAME=$(hostname)
 FQDN=$(hostname -f)
 DATE=$(date +%F)
 
-# Append only once (idempotent)
-if ! grep -q "Autoscaling Instance Details" /var/www/html/index.nginx-debian.html; then
-  sudo tee -a /var/www/html/index.nginx-debian.html > /dev/null <<EOF
+sudo tee -a /var/www/html/index.nginx-debian.html > /dev/null <<EOF
 <hr>
 <div style="color: white;">
   <h2>Autoscaling Instance Details</h2>
@@ -27,6 +22,5 @@ if ! grep -q "Autoscaling Instance Details" /var/www/html/index.nginx-debian.htm
   <p>Server IP Address is: $IP</p>
 </div>
 EOF
-fi
 
 sudo systemctl reload nginx
