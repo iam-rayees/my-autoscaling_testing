955a4404de767b84c02035768a49bc2add45dec5
Mohd Rayees <mailtomdrayees@gmail.com>
2026-04-12 08:51:58 +0530

    Create commands.sh

---
+++ autoscale_AMIpacker/commands.sh
@@ -0,0 +1,6 @@
+packer.exe validate --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
+
+packer.exe inspect --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
+
+packer.exe build --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
+
