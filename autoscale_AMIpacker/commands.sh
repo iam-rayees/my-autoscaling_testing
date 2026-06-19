e6035209f4b7d32a60ad88db95a53de5a3a340ab
Mohd Rayees <mailtomdrayees@gmail.com>
2026-06-19 18:27:06 +0530

    Add linux packer commands in commands.sh

--- autoscale_AMIpacker/commands.sh
+++ autoscale_AMIpacker/commands.sh
@@ -1,6 +1,15 @@
+## For Windows systems:
 packer.exe validate --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
 
 packer.exe inspect --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
 
 packer.exe build --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
 
+## For Linux systems:
+./packer validate --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
+
+./packer inspect --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
+
+./packer build --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
+
+packer init packer.pkr.hcl
