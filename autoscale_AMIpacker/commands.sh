2d0f89f60b50d1fc7805f3245120230ed71716ab
Mohd Rayees <mailtomdrayees@gmail.com>
2026-06-19 18:28:20 +0530

    Update commands.sh-2

--- autoscale_AMIpacker/commands.sh
+++ autoscale_AMIpacker/commands.sh
@@ -6,10 +6,11 @@ packer.exe inspect --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
 packer.exe build --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
 
 ## For Linux systems:
-./packer validate --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
-
-./packer inspect --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
-
-./packer build --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
-
 packer init packer.pkr.hcl
+
+packer validate --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
+
+packer inspect --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
+
+packer build --var-file=packer.auto.pkrvars.hcl packer.pkr.hcl
+
